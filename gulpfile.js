97a76a3ee08328480a1b7fffffc270516a4194ad
Bruno <brunodossantosportela@gmail.com>
2023-10-29 00:25:43 -0300

    Adicionando imagens

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify')
 const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
 
 
 function compilaSass() {
@@ -15,10 +16,14 @@ function comprimeJs() {
     return gulp.src('./src/js/*.js').pipe(uglify()).pipe(obfuscate()).pipe(gulp.dest('./dist/js'));
 }
 
+function comprimeImage() {
+    return gulp.src('./src/img/*').pipe(imagemin()).pipe(gulp.dest('./dist/img'))
+}
+
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass, comprimeJs))
 }
 
-exports.default = gulp.parallel(compilaSass, comprimeJs);
+exports.default = gulp.parallel(compilaSass, comprimeJs, comprimeImage);
 

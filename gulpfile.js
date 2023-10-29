2ef4b2504742a786c3e9d594af2742bf4ce1561e
Bruno <brunodossantosportela@gmail.com>
2023-10-29 00:05:12 -0300

    Comprimindo JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify')
+const obfuscate = require('gulp-obfuscate')
+
 
 function compilaSass() {
     return gulp.src('./src/styles/main.scss').pipe(sourcemaps.init()).pipe(sass({
@@ -8,9 +11,14 @@ function compilaSass() {
     })).pipe(sourcemaps.write('./maps')).pipe(gulp.dest('./dist/styles/'));
 }
 
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass))
+function comprimeJs() {
+    return gulp.src('./src/js/*.js').pipe(uglify()).pipe(obfuscate()).pipe(gulp.dest('./dist/js'));
 }
 
-exports.default = gulp.parallel(compilaSass);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass, comprimeJs))
+}
+
+exports.default = gulp.parallel(compilaSass, comprimeJs);
 
